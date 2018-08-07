273ce103ea495aa473598f802eca733a39d3862d
Marcelo Miranda <mmiranda@ciandt.com>
2018-08-07 20:54:36 -0300

    teste porta heroku

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/my-music-web'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/your_app_name/index.html'));
+app.get('/*', function (req, res) {
+
+    res.sendFile(path.join(__dirname, '/dist/your_app_name/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
 //app.listen(process.env.PORT || 8080);
 
-log.Fatal(app.listen(process.env.PORT))
+app.listen(config.port, function () {
+    console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+});
