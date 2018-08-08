fb492f0b0086ea55295685b836bb22eea6ee4163
Marcelo Miranda <mmiranda@ciandt.com>
2018-08-07 21:15:36 -0300

    teste heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static('./dist/my-music-web'));
 
 app.get('/*', function (req, res) {
 
-    res.sendFile(path.join(__dirname, '/dist/your_app_name/index.html'));
+    res.sendFile(path.join(__dirname, '/dist/my-music-web/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
