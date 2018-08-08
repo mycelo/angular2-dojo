a27c644a2746a6bb9e3290cbb375ce043e92bbc3
Marcelo Miranda <mmiranda@ciandt.com>
2018-08-07 21:50:43 -0300

    ajuste portas

--- src/app/shared/music.service.ts
+++ src/app/shared/music.service.ts
@@ -10,32 +10,33 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class MusicService {
 	
-	private serviceUrl: string = 'http://localhost:9091/api/';
+	private serviceMusicUrl: string = 'http://localhost:9091/api/';
+	private servicePlaylistUrl: string = 'http://localhost:9092/api/';
 
 	constructor(private http: Http) {}
 
 	getMusicas(params: string): Observable<Musica[]> {
 
-		return this.http.get(this.serviceUrl +'musicas/'+this.resolveUrl('filtro', params))
+		return this.http.get(this.serviceMusicUrl +'musicas/'+this.resolveUrl('filtro', params))
 					.map((res: Response) => res.json().map((musica: Musica) => new Musica().deserialize(musica)));
 	}
 
 	getPlaylist(params: string): Observable<Playlist> {
 
-		return this.http.get(this.serviceUrl +'playlists/'+this.resolveUrl('user', params))
+		return this.http.get(this.servicePlaylistUrl +'playlists/'+this.resolveUrl('user', params))
 					.map((playlist: Response) => new Playlist().deserialize(playlist.json()));
 	}
 
 	putPlaylist(playlistId: string): Observable<Response> {
 
-		let requestUrl: string = this.serviceUrl + 'playlists/'+playlistId+'/musicas';
+		let requestUrl: string = this.servicePlaylistUrl + 'playlists/'+playlistId+'/musicas';
 		console.log("Performing PUT request on: " + requestUrl);
 		return this.http.put(requestUrl, {});
 	}
 
 	deleteFromPlaylist(playlistId: string, musicaId: string): Observable<Response> {
 
-		let requestUrl: string = this.serviceUrl + 'playlists/'+playlistId+'/musicas/'+musicaId;
+		let requestUrl: string = this.servicePlaylistUrl + 'playlists/'+playlistId+'/musicas/'+musicaId;
 		console.log("Performing DELETE request on: " + requestUrl);
 		return this.http.delete(requestUrl);
 	}
